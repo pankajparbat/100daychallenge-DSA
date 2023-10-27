d6b1540d1f7b0ab9d53272a2648f9cbe4578b021
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2023-10-27 19:18:55 +0530

    number is even or odd by using bitwise opretor

---
+++ findoutenvenorodd.java
@@ -0,0 +1,13 @@
+public class findoutenvenorodd {
+    public static void main(String[] args) {
+        int N=3;
+        if ((N & 1)==1)
+        {
+            System.out.println("odd");
+        }
+        else {
+            System.out.println("event");
+        }
+        
+    }
+}
